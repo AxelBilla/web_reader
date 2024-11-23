e5a025c0ab3abbbbb6bc99c4f4b921ef0790ab91
AxelBilla <144888254+AxelBilla@users.noreply.github.com>
2024-11-23 21:43:30 +0100

    Add files via upload

--- assets/js/reader.js
+++ assets/js/reader.js
@@ -27,8 +27,28 @@ function readFiles() {
       document.getElementById("upload-box").style.display= "none";
     }
   }
-  
   const picker = document.querySelector("#upload");
   picker.addEventListener("change", readFiles);
 
+  document.getElementById("clear-button").addEventListener('click', function bike() {
+    document.getElementById("clear-button-warning-box").style.display= "flex";
+    document.getElementById("clear-button-warning-bg").style.display= "flex";
+  });
+  
+  document.getElementById("clear-button-warning-bg").addEventListener('click', function mike() {
+    document.getElementById("clear-button-warning-box").style.display= "none";
+    document.getElementById("clear-button-warning-bg").style.display= "none";
+    document.getElementById('settings-content').style.marginTop  = "-5.5em";
+    document.getElementById('settings-content').style.minHeight = "80%"
+  });
+  
+  document.getElementById("clear-button-warning-btn").addEventListener('click', function dike() {
+    document.querySelector("#content-images").replaceChildren();
+    document.getElementById("upload-box").style.display= "flex";
+    document.getElementById("clear-button-warning-box").style.display= "none";
+    document.getElementById("clear-button-warning-bg").style.display= "none";
+    document.getElementById('settings-content').style.marginTop  = "-5.5em";
+    document.getElementById('settings-content').style.minHeight = "80%"
+  
+  });
   
